7f22a44febf668fc77d52b25b90e774c6878fc05
黄腾飞 <htfa1991@foxmail.com>
2016-01-18 14:52:59 +0800

    Initial Commit

---
+++ ios-map-share/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-map-share
+//
+//  Created by nick on 16/1/18.
+//  Copyright © 2016年 nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios-map-share/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ios-map-share
+//
+//  Created by nick on 16/1/18.
+//  Copyright © 2016年 nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
